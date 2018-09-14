2f728d781fc5f94fae80b801a887fb95fbe4805d
MarlenaFlueh <mf@esyoil.com>
2018-09-14 08:19:53 +0200

    go into test mode

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const sendData = async temperature => {
 };
 
 const runEveryHour = () => {
-  setTimeout(executeFunctions, 3600000);
+  setTimeout(executeFunctions, 600000);
 };
 
 const executeFunctions = async () => {
