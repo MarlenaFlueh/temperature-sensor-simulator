35cb2c0e2d3f6508a0ca654ac63d600eb05678ac
MarlenaFlueh <mf@esyoil.com>
2018-09-24 11:14:41 +0200

    change back time set out

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const sendData = async temperature => {
 };
 
 const runEveryHour = () => {
-  setTimeout(executeFunctions, 60000);
+  setTimeout(executeFunctions, 60 * 60000);
 };
 
 const executeFunctions = async () => {
